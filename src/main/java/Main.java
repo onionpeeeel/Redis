4667c96fd7165bd3cd66900fecf0465c9bf6e56d
OJS <onionpeeel@gmail.com>
2024-03-25 11:02:21 +0900

    Respond to PING

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,38 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+  public final static String OK = "+PONG\r\n";
   public static void main(String[] args){
 //     You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-//      Uncomment this block to pass the first stage
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
     int port = 6379;
     try {
+      // 서버 생성
       serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
       serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
+      // Client 접속 Accept
       clientSocket = serverSocket.accept();
+
+      // client 가 보낸 message
+      // DataInputStream is = new DataInputStream(clientSocket.getInputStream());
+      BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+      // client 에 data 전송
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+      bw.write(OK);
+      bw.newLine();
+      bw.flush();
+      bw.close();
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     } finally {
