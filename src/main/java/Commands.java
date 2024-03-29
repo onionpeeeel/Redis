2865e439efe27e137b9460e7da2dbc3eca1b151e
OJS <onionpeeel@gmail.com>
2024-03-29 11:39:28 +0900

    Implement the SET & GET Commands

---
+++ src/main/java/Commands.java
@@ -0,0 +1,9 @@
+public class Commands {
+    public static final String ECHO = "echo";
+
+    public static final String PING = "ping";
+
+    public static final String SET = "set";
+
+    public static final String GET = "get";
+}
--- src/main/java/ThreadRunnable.java
+++ src/main/java/ThreadRunnable.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -46,54 +47,40 @@ public class ThreadRunnable implements Runnable {
     public void multiConnect() {
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String input = br.readLine();
 
-            while (true) {
-                String readline = br.readLine();
-                try {
-                    if (readline.contains("ping")) {
-                        clientSocket.getOutputStream().write(PONG);
-                    } else if (readline.contains("echo")) {
-                        while (true) {
-                            readline = br.readLine();
-                            if (readline.startsWith("$")) {
-                                continue;
-                            }
-                            clientSocket.getOutputStream().write(returnCommand(readline, "simple"));
-                        }
-
-                    } else if (readline.contains("set")) {
-                        while (true) {
-                            readline = br.readLine();
-                            if (readline.startsWith("$")) {
-                                continue;
-                            } else {
-                                String key = readline;
-                                readline = br.readLine();
-                                if (readline.startsWith("$")) {
-                                    readline = br.readLine();
-                                    if (!readline.startsWith("$")) {
-                                        String value = readline;
-                                        commandSave(key, value);
-                                        clientSocket.getOutputStream().write(OK);
-                                    }
-                                }
-                            }
-                        }
-                    } else if (readline.equals("get")) {
-                        while (true) {
-                            readline = br.readLine();
-                            if (readline.startsWith("$")) {
-                                continue;
-                            } else {
-                                System.out.println(readline);
-                                if (commandList.containsKey(readline)) {
-                                    clientSocket.getOutputStream().write(returnCommand(commandList.get(readline), "bulk"));
-                                }
-                            }
-                        }
+            while (input != null && !input.isEmpty()) {
+                if (input.startsWith("*")) {
+                    int numOfLines = Integer.parseInt(String.valueOf(input.charAt(1)));
+                    ArrayList<String> storedCommand = new ArrayList<>(numOfLines * 2);
+                    for (int i = 0; i < numOfLines * 2; i++) {
+                        storedCommand.add(br.readLine());
                     }
-                } catch (Exception e) {
-                    break;
+                    String command = storedCommand.get(1);
+                    switch (command.toLowerCase()) {
+                        case Commands.PING:
+                            clientSocket.getOutputStream().write(PONG);
+                            break;
+                        case Commands.ECHO:
+                            clientSocket.getOutputStream().write(returnCommand(storedCommand.get(3), "simple"));
+                            break;
+                        case Commands.SET:
+                            String key = storedCommand.get(3);
+                            String value = storedCommand.get(5);
+                            commandList.put(key, value);
+                            clientSocket.getOutputStream().write(OK);
+                            break;
+                        case Commands.GET:
+                            String getKey = storedCommand.get(3);
+                            String getValue = commandList.get(getKey);
+
+                            if (getValue == null) {
+                                clientSocket.getOutputStream().write("$-1\r\n".getBytes(StandardCharsets.UTF_8));
+                            } else {
+                                clientSocket.getOutputStream().write(returnCommand(getValue, "bulk"));
+                            }
+                            break;
+                    }
                 }
             }
         } catch (IOException e) {
