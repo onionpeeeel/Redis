ee6f830227d4877dd8f1519307c211915a2f35e3
OJS <onionpeeel@gmail.com>
2024-04-02 16:43:57 +0900

    Send handshake (1/3)

--- src/main/java/Commands.java
+++ src/main/java/Commands.java
@@ -10,4 +10,10 @@ public class Commands {
     public static final String PX = "px";
 
     public static final String INFO = "info";
+
+    public static String replconf(int reqPort) {
+        String port = String.valueOf(reqPort);
+
+        return "*3\r\n$8\r\nREPLCONF\r\n$14\r\nlistening-port\r\n$4\r\n" + port + "\r\n";
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,6 +43,8 @@ public class Main {
         clientSocket = new Socket(redisProperties.getMasterNode(),
         Integer.parseInt(redisProperties.getMasterPort()));
         clientSocket.getOutputStream().write("*1\r\n$4\r\nping\r\n".getBytes(StandardCharsets.UTF_8));
+        clientSocket.getOutputStream().write(Commands.replconf(port).getBytes(StandardCharsets.UTF_8));
+        clientSocket.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes(StandardCharsets.UTF_8));
       }
 
       while (true) {
