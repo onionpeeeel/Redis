d35d7a2bf8bf0319a7c2915f2b71b738ff7aac11
OJS <onionpeeel@gmail.com>
2024-04-05 14:50:57 +0900

    Send handshake (3/3)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -43,6 +43,10 @@ public class Main {
         clientSocket = new Socket(redisProperties.getMasterNode(),
         Integer.parseInt(redisProperties.getMasterPort()));
         System.out.println("--------------------------1");
+        clientSocket.getOutputStream().write("*1\r\n$4\r\nping\r\n".getBytes(StandardCharsets.UTF_8));
+        clientSocket.getOutputStream().write(Commands.replconf(port).getBytes(StandardCharsets.UTF_8));
+        clientSocket.getOutputStream().write("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n".getBytes(StandardCharsets.UTF_8));
+        clientSocket.getOutputStream().write("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n".getBytes(StandardCharsets.UTF_8));
       }
 
       while (true) {
